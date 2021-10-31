f261c4d98f5f1c68ad630d9cdf80b5b7cc6e8c98
Gabriel Oliveira <gabriel.oliveira@ikatec.com.br>
2021-10-31 16:39:49 -0300

    feat(model): add model Pet

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,40 +1,54 @@
 import { Request, Response}  from 'express';
+
+import { Pet } from '../models/Pet';
 import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req:Request, res:Response) => {
+    let list = Pet.getAll();
+
     res.render('pages/main',{
         menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     });
 }
 
 export const dogs = (req:Request, res:Response) => {
+    let list = Pet.getFromType('dog');
+
     res.render('pages/main',{
         menu: createMenuObject('dog'),
         banner: {
             title: 'Todos os cachorros',
             background: 'banner_dog.jpg'
-        }
+        },
+        list
     });}
 
 export const cats = (req:Request, res:Response) => {
+    let list = Pet.getFromType('cat');
+
     res.render('pages/main',{
         menu: createMenuObject('cat'),
         banner: {
             title: 'Todos os gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 
 export const fishes = (req:Request, res:Response) => {
+    let list = Pet.getFromType('fish');
+
     res.render('pages/main',{
         menu: createMenuObject('fish'),
         banner: {
             title: 'Todos os peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
     });}
